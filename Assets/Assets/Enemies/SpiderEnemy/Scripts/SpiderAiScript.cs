f97122990362186bd940d2888832c21f8ad2b442
Simojian <simon.kondela@gmail.com>
2023-04-15 19:38:59 +0200

    Spider Ai Updated

--- Assets/Assets/Enemies/SpiderEnemy/Scripts/SpiderAiScript.cs
+++ Assets/Assets/Enemies/SpiderEnemy/Scripts/SpiderAiScript.cs
@@ -9,12 +9,15 @@ public class SpiderAiScript : MonoBehaviour
     private NavMeshAgent aiNavMesh;
     private Collider triggerCol;
     public GameObject player;
+    public GameObject enemy;
     private bool playerCheck;
 
     float timer, wanderTime;
     
     public float movementRange;
+    public float movementRatio;
     public Vector2 wanderTimerRange;
+    public float minDistance;
         
 
     public enum State
@@ -23,6 +26,8 @@ public class SpiderAiScript : MonoBehaviour
         Wander,
         Idle,
         FindZombie,
+        TouchCharacter,
+        TouchZombie
     }
 
     private State currentState;
@@ -41,6 +46,13 @@ public class SpiderAiScript : MonoBehaviour
         switch (currentState)
         {
             case State.FindCharacter:
+                timer += Time.deltaTime;
+                MoveTowards(player);
+                float distToPlayer = Vector3.Distance(player.transform.position, transform.position);
+                if (distToPlayer < minDistance)
+                {
+                    currentState = State.TouchCharacter;
+                }
                 break;
             case State.Wander:
                 timer += Time.deltaTime;
@@ -49,6 +61,19 @@ public class SpiderAiScript : MonoBehaviour
             case State.Idle:
                 break;
             case State.FindZombie:
+                timer += Time.deltaTime;
+                MoveTowards(enemy);
+                float dist = Vector3.Distance(enemy.transform.position, transform.position);
+                if (dist < minDistance)
+                {
+                    currentState = State.TouchZombie;
+                }
+                break;
+            case State.TouchCharacter:
+                MoveTowardsStraight(player);
+                break;
+            case State.TouchZombie:
+                MoveTowardsStraight(enemy);
                 break;
             
         }
@@ -58,7 +83,7 @@ public class SpiderAiScript : MonoBehaviour
     {
         if(triggerCol.gameObject.tag == "Player")
         {
-            playerCheck = true;
+            currentState = State.FindCharacter;
         }
     }
 
@@ -76,8 +101,27 @@ public class SpiderAiScript : MonoBehaviour
         }
     }
 
+    void MoveTowards(GameObject followObject)
+    {
+        if (timer >= wanderTime)
+        {
+            Vector2 wanderTarget = Random.insideUnitCircle * movementRange;
+            Vector3 wanderPos3D = new Vector3(((1 - movementRatio)*transform.position.x + movementRatio*followObject.transform.position.x) + wanderTarget.x, transform.position.y, ((1 - movementRatio)*transform.position.z + movementRatio*followObject.transform.position.z) + wanderTarget.y);
+            SetAITarget(wanderPos3D);
+            timer = 0;
+            wanderTime = Random.Range(wanderTimerRange.x, wanderTimerRange.y);
+            
+            SetAITarget(wanderPos3D);
+        }
+    }
+
     void SetAITarget(Vector3 targetLocation)
     {
         aiNavMesh.SetDestination(targetLocation);
+    }
+
+    private void MoveTowardsStraight(GameObject target)
+    {
+        aiNavMesh.SetDestination(target.transform.position);
     }
 }
